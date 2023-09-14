19bba23481f09125549484a3be79ed261928b49a
Ara Ventura <ara.venturafitch@gmail.com>
2023-09-13 22:21:08 -0700

    Update ImagePopup.jsx

--- src/components/ImagePopup.jsx
+++ src/components/ImagePopup.jsx
@@ -1,20 +1,22 @@
-function ImagePopup() {
-    return (
-        
-        <div class="cover">
-        <div class="cover__wrapper">
-          <div class="cover__close-button" id="cover-close-button">
-            <img
-              src="<%=require('./images/CloseIcon.png')%>"
-              alt="Closing Icon"
-            />
-          </div>
-          <img class="cover__image" src="#" alt="Cover Backdrop" />
-          <h3 class="cover__subtitle"></h3>
+import ClosingIcon from "../images/CloseIcon.png";
+
+function ImagePopup(props) {
+  return (
+    <div className="cover">
+      <div className="cover__wrapper">
+        <div className="cover__close-button" id="cover-close-button">
+          <img onClick={props.onClose} src={ClosingIcon} alt="Closing Icon" />
         </div>
-        <div class="cover__backdrop"></div>
+        <img
+          className="cover__image"
+          src={props.selectedCard.link}
+          alt="Cover Backdrop"
+        />
+        <h3 className="cover__subtitle"></h3>
       </div>
-    )
+      <div className="cover__backdrop"></div>
+    </div>
+  );
 }
 
 export default ImagePopup;
