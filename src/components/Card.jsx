a7181d5b3a8226547353a299c7d6cb46104d5a6c
Ara Ventura <ara.venturafitch@gmail.com>
2023-09-13 22:20:17 -0700

    Create Card.jsx

---
+++ src/components/Card.jsx
@@ -0,0 +1,30 @@
+import TrashIcon from "../images/trash.png";
+
+function Card({ card, onCardClick }) {
+  function handleClick() {
+    onCardClick(card);
+  }
+
+  return (
+    <li className="card__item">
+      <button className="card__trash">
+        <img src={TrashIcon} alt="Trash Button" />
+      </button>
+      <img
+        onClick={handleClick}
+        className="card__image"
+        src={card.link}
+        alt={card.name}
+      />
+      <div className="card__wrapper">
+        <span className="card__title">{card.name}</span>
+        <button className="card__like"></button>
+      </div>
+      <div className="card__counter">
+        <span className="card__counter_likes">{card.likes.length}</span>
+      </div>
+    </li>
+  );
+}
+
+export default Card;
